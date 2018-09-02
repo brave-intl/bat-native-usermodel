adb80d7233443ae658eccc87b1214d19f0a6695c
ptigas <ptigas@gmail.com>
2018-09-02 03:27:09 +0100

    added tests

--- test.cc
+++ test.cc
@@ -16,24 +16,73 @@
 #include "bag_of_words_extractor.h"
 #include "naive_bayes.h"
 
+#include "deps/bat-native-rapidjson/include/rapidjson/document.h"
+#include "deps/bat-native-rapidjson/include/rapidjson/writer.h"
+#include "deps/bat-native-rapidjson/include/rapidjson/stringbuffer.h"
+
+#include <fstream>
+#include <streambuf>
+
+using namespace rapidjson;
+
+std::string loadFile(std::string filename) {
+  std::ifstream t(filename);
+  std::string data((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+
+  return data;  
+}
+
+Document loadJson(std::string filename) {
+  auto json = loadFile(filename);
+
+  Document d;
+  d.Parse(json.c_str());
+
+  return d;
+}
+
 int main(int argc, char* argv[]) {
   printf("Testing bat-native-usermodel...\n");
 
-  usermodel::BagOfWords bow;
-
-  std::string str = std::string("The Comme Up: David Bullock (Founder of 907 Agency) #Mood: Most Hilarious Hip-Hop Memes And GIFs For The 'G' Challenge On The Green Carpet Rate the Bars With N.O.R.E. FWD: Ella Mai Is the Soulstress of the Future Hip-Hop Hates Me: The Complexity in Being a Woman ‘For the Culture’ Exclusive: Watch Halle Berry React to Trey Songz Sliding Into Her DMs ‘for the Culture’ Detroit: Anthony Mackie Jada Pinkett Smith and Queen Latifah Talk Reuniting on Film In Bed With Hennessy Almanzar Gabby Sidibe Test Drives Fenty Beauty to Prove It's For All Skin Tones I Got Non-Invasive Plastic Surgery to Cheat My Way to a Toned Tummy Serena Williams Is Sharing Her Pregnancy Story And Introduces The World To Her Daughter Alexis See The Fashion Line Danielle Brooks Is Designing For All The BBWs Around The World Solange Just Went Platinum Blonde Maxine Waters Reclaiming Her Time Just Became Everyone's New Summer '17 Mantra Serena Williams Laid Out the Blueprint on 'How Black Women Can Close the Pay Gap' in This Moving Essay Terrifying Viral Video Shows Ohio State Fair Ride Malfunction in Mid-Air, Killing One and Injuring Many Face Value Hip Hop Awards 17 50 Central FACE VALUE HIP HOP AWARDS 50 CENTRAL Check out the freestyling gurus featured in the cypher! See everything we show you about the awards. Hot weather. Hot bars. There's a lotta love and mad hip-hop. THE QUEEN OF LATE NIGHT IS HERE 0 DAYS 0 HRS 0 MIN 0 SEC days Follow us Subscribe to our Newsletter Music Janet Jackson’s ‘State of the World’ Tour Proves Iconic Doesn’t Equal Ignorance Hip-Hop Hates Me: Women & the Culture FWD: THEY. Will Spark A Change in Music The Cypher Takeover The Best And Funniest Moments From Hip-Hop Awards '17 See Who Blessed The Hip-Hop Awards Instabooth See Where Cardi B, 2 Chainz, Keyshia Cole And More Will Sit Subscribe to our Newsletter  Latest in music Follow us Subscribe to our Newsletter HIP HOP AWARDS 2017 TUES, OCT 10 8P/7C HOST DJ KHALED");
-  bow.process(str);
-  
+  // create model  
   usermodel::NaiveBayes mnb;
-  mnb.loadModel("{\"classes\":[\"a\", \"b\", \"c\", \"d\"],\"priors\":[-0.2, -1.1, -2.2, -3.3],\"logProbs\": {\"test\": [1.0, 2.0, 3.0, 4.0],\"there\": [1.0, 2.2, 3.0, 4.0]}}");
+  mnb.loadModel(loadFile("model.json"));
 
-  for (auto c : mnb.classes()) {
-    std::cout << "Class: " + c << std::endl;
+
+  auto doc = loadJson("data/predictions.json");
+  const Value& data = doc["data"]; // Using a reference for consecutive access is handy and faster.
+  for (SizeType it = 0; it < data.Size(); it++) {
+    std::string label = data[it]["label"].GetString();
+    std::string filename = data[it]["doc"].GetString();
+
+    auto doc = loadFile(std::string("data/") + std::string(filename));
+
+    // load features
+    usermodel::BagOfWords bow;
+    bow.process(doc);
+
+    auto res = mnb.predict(bow.getFrequencies());
+    int i = 0;
+    int argmax = 0;
+    double max = 0.0;
+    for (auto c: res) {
+      if (c > max) {
+        argmax = i;
+        max = c;
+      }
+      i++;
+      //std::cout << mnb.classes().at(i++) << " " << c << std::endl;
+    }
+
+    std::cout << filename << " - " << ((mnb.classes().at(argmax) == label) ? "PASS" : "FAIL") << std::endl;
+    if (mnb.classes().at(argmax) != label) {
+      std::cout << "\t" << mnb.classes().at(argmax) << " - " << label << std::endl;
+      std::cout << "FEATURES" << std::endl;
+      for (auto f: bow.getFrequencies()) {
+        std::cout << "\t" << f.first << std::endl;
+      }
+    }
   }
 
-  auto res = mnb.predict(bow.getFrequencies());
-  for (auto c: res) {
-    std::cout << c << std::endl;
-  }
   return 0;
 }
