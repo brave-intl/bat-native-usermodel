fa64a3bdfbb10ba7ed9f50157f7682320d1a571a
ptigas <ptigas@gmail.com>
2018-09-04 15:28:24 +0100

    updated license

--- src/user_profile.cc
+++ src/user_profile.cc
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include "user_profile.h"
 
 bool usermodel::UserProfile::update(std::vector<double> scores, const std::string& url) {
