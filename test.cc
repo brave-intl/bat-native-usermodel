3980217117e64adae0c5b4ece77488c62ebad41d
ptigas <ptigas@gmail.com>
2018-09-04 14:33:47 +0100

    added catch framework for testing

--- test.cc
+++ test.cc
@@ -25,6 +25,10 @@
 #include <fstream>
 #include <streambuf>
 
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
 using namespace rapidjson;
 
 std::string loadFile(std::string filename) {
@@ -43,20 +47,16 @@ Document loadJson(std::string filename) {
   return d;
 }
 
-int main(int argc, char* argv[]) {
-  printf("Testing bat-native-usermodel...\n");
-
+TEST_CASE( "Test logistic regression", "[classifier]" ) {
   usermodel::LogisticRegression cl;
   cl.LoadModel("{\"features\":[\"test1\", \"test2\", \"test3\"], \"weights\":[0.2, 0.4, 0.9], \"bias\": 0.5}");
   auto features = std::map<std::string, double>();
   features["test1"] = 0.1;
   features["test2"] = 0.3;
-  std::cout << "results = " << cl.Predict(features).at(0) << std::endl;
+  REQUIRE(cl.Predict(features).at(0) ==  Approx( 0.65475 ));
+}
 
-  // test naive bayes
-  std::cout << "TESTING NAIVE BAYES" << std::endl;
-
-  // create model  
+TEST_CASE( "Test naive bayes", "[classifier]" ) {
   usermodel::UserModel um;
   um.initializePageClassifier(loadFile("model.json"));
 
@@ -69,12 +69,6 @@ int main(int argc, char* argv[]) {
     auto doc = loadFile(std::string("data/") + std::string(filename));
     auto scores = um.classifyPage(doc);
     auto predicted = um.winningCategory(scores);
-
-    std::cout << filename << " - " << ((predicted == label) ? "PASS" : "FAIL") << std::endl;
-    if (predicted != label) {
-      std::cout << "\t" << predicted << " - " << label << std::endl;
-    }
+    REQUIRE( predicted == label );
   }
-
-  return 0;
 }
