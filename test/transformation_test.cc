2f278e183d14dc488ad4dbdb7fd575b2cd50548a
dimmu <dathanasakis@gotbim.com>
2019-04-04 15:35:33 +0100

    passing transformation test

--- test/transformation_test.cc
+++ test/transformation_test.cc
@@ -2,17 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
+#include "brave/vendor/bat-native-usermodel/src/transformation.h"
+// #include "transformation.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace usermodel {
 
 // Test data directory, relative to source root
-const base::FilePath::CharType kTestDataRelativePath[] =
-  FILE_PATH_LITERAL("brave/vendor/bat-native-usermodel/test/data");
+// const base::FilePath::CharType kTestDataRelativePath[] =
+//   FILE_PATH_LITERAL("brave/vendor/bat-native-usermodel/test/data");
 
-class TraansformationTest : public ::testing::Test {
+class TransformationTest : public ::testing::Test {
  protected:
   TransformationTest() {
     // You can do set-up work for each test here
@@ -43,10 +44,12 @@ TEST_F(TransformationTest, ToLowerTest) {
   std::string lower_case = "lower case";
   auto upper_datapoint = usermodel::Data_point(upper_case);
   usermodel::To_lower to_lower;
-  EXPECT_TRUE(to_lower.apply(upper_datapoint));
+  // EXPECT_TRUE(to_lower.apply(upper_datapoint));
+  to_lower.apply(upper_datapoint);
+  
   auto lower_datapoint=to_lower.get();
   EXPECT_EQ(0, lower_datapoint.type);
-  EXPECCT_EQ(0, lower_case.compare(lower_datapoint.data_text))
+  EXPECT_EQ(0, lower_case.compare(lower_datapoint.data_text));
 }
 
 }  // namespace usermodel
