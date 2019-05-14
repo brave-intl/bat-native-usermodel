ba5e09ae4d8ce48877b7142634640ff4a5fbeb0c
dimmu <dathanasakis@gotbim.com>
2019-05-15 00:24:55 +0100

    adding pipelines class

--- src/pipeline.cc
+++ src/pipeline.cc
@@ -5,17 +5,26 @@
 namespace usermodel{
     Pipeline::Pipeline(){}
     Pipeline::~Pipeline() = default;
+    Pipeline::Pipeline(Pipeline &other){
+        transformations = other.transformations;
+        classifier = other.classifier;
+    }
     Pipeline::Pipeline(std::list<Transformation> representation,  Classifier c){
         transformations = representation;
         classifier = c;
     }
+    Pipeline::Pipeline(std::list<Transformation> representation){
+        transformations = representation;
+    }
     std::map<std::string, float> Pipeline::apply(Data_point &inp){
         Data_point last_point = Data_point(inp);
           for (std::list<Transformation>::iterator it = transformations.begin(); it != transformations.end(); ++it){
-              auto tmp = it->apply(last_point);
-              if (tmp){
-                last_point = Data_point(it->get());
-              }          
+            //   auto tmp = it->apply(last_point);
+            //   if (tmp){
+            //     last_point = Data_point(it->get());
+            //   }          
+            it->apply(last_point);
+            last_point = Data_point(it->get());
           }
         return classifier.Predict(last_point);
 
--- src/pipeline.h
+++ src/pipeline.h
@@ -10,8 +10,10 @@ namespace usermodel{
 class Pipeline{
     public:
         Pipeline();
+        Pipeline(Pipeline &other);
         Pipeline(std::list<Transformation> representation,  Classifier c);
         ~Pipeline();
+        Pipeline(std::list<Transformation> representation);
         // Pipeline(std::string json_spec);
         // Pipeline() --> protobuf equiv
         std::map<std::string, float> apply(Data_point &inp);
