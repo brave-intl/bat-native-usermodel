54064ec113b28c11be6167c0fb764a870d479709
dimmu <dathanasakis@gotbim.com>
2019-05-16 14:34:49 +0100

    clean up couts

--- src/pipeline.cc
+++ src/pipeline.cc
@@ -17,13 +17,7 @@ namespace usermodel{
         Data_point last_point = Data_point(inp);
         for (unsigned i = 0; i < transformations.size(); i++){
             last_point = transformations[i].get(last_point);
-            std::cout << "after loop:  " << i << " type is: "<< last_point.type<< '\n';
         }
-        std::cout << "after final loop type is: "<< last_point.type<< '\n';
-        if (last_point.type == sparse_vector){
-            std::cout << "and dimensionality is: " << last_point.n_dims<<'\n';
-            std::cout <<"while c++ dims are:"<< last_point.data_sparse.size()<<'\n';
-        } 
         return classifier.Predict(last_point);
 
     }
